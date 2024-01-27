16072b8634e73f8a753a46f94595526dbf3b082a
Aman Relan <aman.relan-ext@jci.com>
2024-01-28 02:49:24 +0530

    atom-family, asynchronous-data-query, selector-family

---
+++ asynchronous-data-query/src/App.jsx
@@ -0,0 +1,12 @@
+import { RecoilRoot } from "recoil";
+import MainApp from "./MainApp";
+
+function App() {
+  return (
+    <RecoilRoot>
+      <MainApp />
+    </RecoilRoot>
+  );
+}
+
+export default App;
